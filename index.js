864af061c357977f607c1d7f06e8ed463ac75236
Sibusiso <sibusisoj@thedigitalacademy.co.za>
2019-01-12 22:36:45 +0200

    running server with hello world

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require("express") ; 
 const app = express() ; 
+
+
+app.listen(3000,()=>{
+    console.log("why me") ; 
+});
+
+app.get("/",(res,req)=>{
+    req.send("hello world") ; 
+})
