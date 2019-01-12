7f99656ea4d2341a18a7343134437042cae096db
Sibusiso <sibusisoj@thedigitalacademy.co.za>
2019-01-12 23:03:27 +0200

    added comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express") ; 
 const app = express() ; 
 
-
+//port to listen to 
 app.listen(3000,()=>{
     console.log("why me") ; 
 });
 
+//route to go to and display following message
 app.get("/",(res,req)=>{
     req.send("hello world") ; 
 })
